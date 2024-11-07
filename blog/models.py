a9fdbc1e2f4765def939470dcdafda5b2e846c40
Ashique A B <ab.ashique10@gmail.com>
2024-11-08 03:30:23 +0530

    added files

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", args=[str(self.id)])
+    
